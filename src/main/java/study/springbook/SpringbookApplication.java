34f968f612f0e7ef9d841352f5237acb4e3ec9ee
goomon <jaegoo.jho@gmail.com>
2023-07-13 17:50:46 +0900

    first commit

---
+++ src/main/java/study/springbook/SpringbookApplication.java
@@ -0,0 +1,14 @@
+package study.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbookApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
