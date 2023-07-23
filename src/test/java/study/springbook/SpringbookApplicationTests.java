439567396cfbb153b148d4be04106ad27d3378d7
goomon <jaegoo.jho@gmail.com>
2023-07-23 21:03:30 +0900

    Chapter7 [Refactor]: 준비 단계

--- src/test/java/study/springbook/SpringbookApplicationTests.java
+++ src/test/java/study/springbook/SpringbookApplicationTests.java
@@ -2,12 +2,8 @@ package study.springbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import study.springbook.factory.TestDaoFactory;
 
 @SpringBootTest
-@ContextConfiguration(classes = TestDaoFactory.class, loader = AnnotationConfigContextLoader.class)
 class SpringbookApplicationTests {
 
 	@Test
