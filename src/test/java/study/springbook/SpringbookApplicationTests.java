1c5120365c9238b90634cb4f76b3c95dbaf5ba55
goomon <jaegoo.jho@gmail.com>
2023-07-24 15:14:01 +0900

    Chapter7 [Refactor]: MaileSender production 버전 분리

--- src/test/java/study/springbook/SpringbookApplicationTests.java
+++ src/test/java/study/springbook/SpringbookApplicationTests.java
@@ -2,8 +2,10 @@ package study.springbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {AppContext.class, TestAppContext.class})
 class SpringbookApplicationTests {
 
 	@Test
